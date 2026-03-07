5d4db3f613d8954595a76e3324a99b0751604975
anjaneyakg <37345426+anjaneyakg@users.noreply.github.com>
2026-03-07 23:54:19 +0530

    Add WealthCreator route to App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,18 @@ import Home from './pages/Home.jsx'
 import Schemes from './pages/Schemes.jsx'
 import LoanVsSIP from './pages/LoanVsSIP.jsx'
 import SIPCalculator from './pages/SIPCalculator.jsx'
+import WealthCreator from './pages/WealthCreator.jsx'
 
 export default function App() {
   return (
     <>
       <Nav />
       <Routes>
-        <Route path="/"               element={<Home />} />
-        <Route path="/schemes"        element={<Schemes />} />
-        <Route path="/loan-vs-sip"    element={<LoanVsSIP />} />
+        <Route path="/"                element={<Home />} />
+        <Route path="/schemes"         element={<Schemes />} />
+        <Route path="/loan-vs-sip"     element={<LoanVsSIP />} />
         <Route path="/sip-performance" element={<SIPCalculator />} />
+        <Route path="/wealth-creator"  element={<WealthCreator />} />
       </Routes>
     </>
   )
