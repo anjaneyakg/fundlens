172d800dca310ba7e2e8bba68945962e26f6dff0
anjaneyakg <37345426+anjaneyakg@users.noreply.github.com>
2026-03-08 00:39:20 +0530

    Add FDCalculator route to App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Schemes from './pages/Schemes.jsx'
 import LoanVsSIP from './pages/LoanVsSIP.jsx'
 import SIPCalculator from './pages/SIPCalculator.jsx'
 import WealthCreator from './pages/WealthCreator.jsx'
+import FDCalculator from './pages/FDCalculator.jsx'
 
 export default function App() {
   return (
@@ -16,6 +17,7 @@ export default function App() {
         <Route path="/loan-vs-sip"     element={<LoanVsSIP />} />
         <Route path="/sip-performance" element={<SIPCalculator />} />
         <Route path="/wealth-creator"  element={<WealthCreator />} />
+        <Route path="/fd-calculator"   element={<FDCalculator />} />
       </Routes>
     </>
   )
