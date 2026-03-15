1f2b4e2d4b74e93767638565c0c522abce4b7cee
anjaneyakg <37345426+anjaneyakg@users.noreply.github.com>
2026-03-15 15:37:13 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home.jsx'
 import Schemes from './pages/Schemes.jsx'
 import CategoryLeaderboard from './pages/CategoryLeaderboard.jsx'
 import STPActual from './pages/STPActual.jsx'
+import CompareSchemes from './pages/CompareSchemes.jsx'
 import LoanVsSIP from './pages/LoanVsSIP.jsx'
 import SIPCalculator from './pages/SIPCalculator.jsx'
 import WealthCreator from './pages/WealthCreator.jsx'
@@ -25,6 +26,7 @@ export default function App() {
         <Route path="/schemes"              element={<Schemes />} />
         <Route path="/category-leaderboard" element={<CategoryLeaderboard />} />
         <Route path="/stp-actual"           element={<STPActual />} />
+        <Route path="/compare-schemes"      element={<CompareSchemes />} />
         <Route path="/loan-vs-sip"          element={<LoanVsSIP />} />
         <Route path="/sip-performance"      element={<SIPCalculator />} />
         <Route path="/wealth-creator"       element={<WealthCreator />} />
