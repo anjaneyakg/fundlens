0982b8a8ae3c7be700b23c8d59f1af3e574815f4
anjaneyakg <37345426+anjaneyakg@users.noreply.github.com>
2026-03-19 20:10:26 +0530

    Update Schemes.jsx

--- src/pages/Schemes.jsx
+++ src/pages/Schemes.jsx
@@ -2017,9 +2017,7 @@ export default function App() {
                       </div>{/* end peer-col-right */}
                       </div>{/* end peer-two-col */}
                     </div>
-                  );
-                })()}
-
+   
                     {/* Fullscreen overlay */}
                     {peerFullscreen && (
                       <div className="peer-overlay"
